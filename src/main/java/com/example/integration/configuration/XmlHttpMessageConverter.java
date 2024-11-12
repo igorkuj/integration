f89c810f2e07377c83e25ed08b8734897ccbcb27
Nikola Davogic <nikola.davogic@panonit.com>
2024-11-12 08:20:29 +0100

    Code cleanup
    - Removed unnecessary null checks
    - Replaced package paths with imports

--- src/main/java/com/example/integration/configuration/XmlHttpMessageConverter.java
+++ src/main/java/com/example/integration/configuration/XmlHttpMessageConverter.java
@@ -4,20 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import org.springframework.util.Assert;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 
+import com.example.integration.model.XmlMessage;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.NonNull;
 
 /**
- * Configuration class for  converting {@link com.example.integration.model.XmlMessage} to {@link org.springframework.http.HttpMessage}.
+ * Configuration class for  converting {@link XmlMessage} to {@link HttpMessage}.
  */
 @Configuration
 @RequiredArgsConstructor
@@ -26,29 +27,27 @@ public class XmlHttpMessageConverter implements HttpMessageConverter<Object> {
     private final XmlMapper xmlMapper;
 
     @Override
-    public boolean canRead(@NonNull Class<?> clazz, org.springframework.http.MediaType mediaType) {
+    public boolean canRead(@NonNull Class<?> clazz, MediaType mediaType) {
         return mediaType != null && mediaType.includes(MediaType.APPLICATION_XML);
     }
 
     @Override
-    public boolean canWrite(@NonNull Class<?> clazz, org.springframework.http.MediaType mediaType) {
+    public boolean canWrite(@NonNull Class<?> clazz, MediaType mediaType) {
         return mediaType != null && mediaType.includes(MediaType.APPLICATION_XML);
     }
 
     @Override
     @NonNull
     public Object read(@NonNull Class<?> clazz, @NonNull HttpInputMessage inputMessage) throws IOException {
-        Assert.notNull(inputMessage, "HttpInputMessage must not be null");
         InputStream inputStream = inputMessage.getBody();
         return xmlMapper.readValue(inputStream, clazz);
     }
 
     @Override
-    public void write(@NonNull Object t, org.springframework.http.MediaType contentType,
+    public void write(@NonNull Object object, MediaType contentType,
                       HttpOutputMessage outputMessage) throws IOException {
-        Assert.notNull(t, "Object must not be null");
         outputMessage.getHeaders().setContentType(contentType);
-        xmlMapper.writeValue(outputMessage.getBody(), t);
+        xmlMapper.writeValue(outputMessage.getBody(), object);
     }
 
     @Override
